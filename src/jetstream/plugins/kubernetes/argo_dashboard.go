71c0394990140ebbb9bfe55ac5b0a89d050ff910
Ethan Apodaca <eapodaca@suse.com>
2019-09-03 13:16:26 -0700

    Use api proxy for argo dashboard

--- src/jetstream/plugins/kubernetes/argo_dashboard.go
+++ src/jetstream/plugins/kubernetes/argo_dashboard.go
@@ -2,6 +2,8 @@ package kubernetes
 
 import (
 	//"context"
+
+	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -53,124 +55,155 @@ func (k *KubernetesSpecification) argoDashboardProxy(c echo.Context) error {
 		return errors.New("Could not get token")
 	}
 
-	log.Debug(tokenRec.AuthToken)
-	log.Debug(tokenRec.AuthType)
+	log.Info(tokenRec.AuthToken)
+	log.Info(tokenRec.AuthType)
 
 	// Make the info call to the SSH endpoint info
 	// Currently this is not cached, so we must get it each time
 	apiEndpoint := cnsiRecord.APIEndpoint
 
-	// target := fmt.Sprintf("%s/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/", apiEndpoint)
-	// target := fmt.Sprintf("%s/api/v1/namespaces/kube-system/services/http:kubernetes-dashboard:/proxy/", apiEndpoint)
-
 	// TODO: Need namespace and whether it is https or http
-	// target := fmt.Sprintf("%s/api/v1/namespaces/kube-system/services/http:kubernetes-dashboard:/proxy/%s", apiEndpoint, path)
-	target := fmt.Sprintf("%s/api/v1/namespaces/argo/services/argo-ui/proxy/%s", apiEndpoint, path)
-	target = fmt.Sprintf("%s/%s", "http://localhost:8081/", path)
-	// target := fmt.Sprintf("%s/%s", apiEndpoint, path)
-	log.Debug(target)
-	targetURL, _ := url.Parse(target)
-	targetURL = normalizeLocation(targetURL)
-
+	target := fmt.Sprintf("%s/api/v1/namespaces/argocd/services/https:argocd-server:https/proxy/%s", apiEndpoint, path)
 	config, err := getConfig(&cnsiRecord, &tokenRec)
 	if err != nil {
 		return errors.New("Could not get config for this auth type")
 	}
 
-	log.Info("Config")
-	log.Info(config.Host)
-	log.Info("Making request")
-	req := c.Request()
-	w := c.Response().Writer
-	log.Info("%v+", req)
+	if len(path) == 0 {
+		log.Info("Looks like we are argo dash root! we will rewrite the html :D")
+		tr, _ := rest.TransportFor(config)
+		client := &http.Client{Transport: tr}
 
-	// if h.tryUpgrade(w, req) {
-	// 	return
-	// }
-	// if h.UpgradeRequired {
-	// 	h.Responder.Error(w, req, errors.NewBadRequest("Upgrade request required"))
-	// 	return
-	// }
-
-	loc := targetURL
-	loc.RawQuery = req.URL.RawQuery
-
-	// If original request URL ended in '/', append a '/' at the end of the
-	// of the proxy URL
-	if !strings.HasSuffix(loc.Path, "/") && strings.HasSuffix(req.URL.Path, "/") {
-		loc.Path += "/"
-	}
-
-	log.Info(loc)
-
-	// From pkg/genericapiserver/endpoints/handlers/proxy.go#ServeHTTP:
-	// Redirect requests with an empty path to a location that ends with a '/'
-	// This is essentially a hack for http://issue.k8s.io/4958.
-	// Note: Keep this code after tryUpgrade to not break that flow.
-	if len(loc.Path) == 0 {
-		log.Info("Redirecting")
-		var queryPart string
-		if len(req.URL.RawQuery) > 0 {
-			queryPart = "?" + req.URL.RawQuery
+		if err != nil {
+			return err
 		}
-		w.Header().Set("Location", req.URL.Path+"/"+queryPart)
-		w.WriteHeader(http.StatusMovedPermanently)
-		return nil
+
+		log.Info("auth: " + config.BearerToken)
+
+		req, _ := http.NewRequest("GET", target, nil)
+		req.Header.Add("Cookie", c.Request().Header.Get("Cookie"))
+
+		resp, err := client.Do(req)
+
+		if err != nil {
+			return err
+		}
+
+		scanner := bufio.NewScanner(resp.Body)
+		var sb strings.Builder
+		for scanner.Scan() {
+			line := scanner.Text()
+			if strings.HasPrefix(line, "    <base") {
+				sb.WriteString(fmt.Sprintf("    <base href=\"%s\">", prefix))
+			} else {
+				sb.WriteString(line + "\n")
+			}
+		}
+		resp.Body.Close()
+
+		c.Response().WriteHeader(200)
+		_, _ = c.Response().Write([]byte(sb.String()))
+	} else {
+
+		log.Info(target)
+		log.Info(fmt.Sprintf("path: `%s`", path))
+		targetURL, _ := url.Parse(target)
+		targetURL = normalizeLocation(targetURL)
+
+		log.Info("Config")
+		log.Info(config.Host)
+		log.Info("Making request")
+		req := c.Request()
+		w := c.Response().Writer
+		log.Info("%v+", req)
+
+		// if h.tryUpgrade(w, req) {
+		// 	return
+		// }
+		// if h.UpgradeRequired {
+		// 	h.Responder.Error(w, req, errors.NewBadRequest("Upgrade request required"))
+		// 	return
+		// }
+
+		loc := targetURL
+		loc.RawQuery = req.URL.RawQuery
+
+		// If original request URL ended in '/', append a '/' at the end of the
+		// of the proxy URL
+		if !strings.HasSuffix(loc.Path, "/") && strings.HasSuffix(req.URL.Path, "/") {
+			loc.Path += "/"
+		}
+
+		log.Info(loc)
+
+		// From pkg/genericapiserver/endpoints/handlers/proxy.go#ServeHTTP:
+		// Redirect requests with an empty path to a location that ends with a '/'
+		// This is essentially a hack for http://issue.k8s.io/4958.
+		// Note: Keep this code after tryUpgrade to not break that flow.
+		if len(loc.Path) == 0 {
+			log.Info("Redirecting")
+			var queryPart string
+			if len(req.URL.RawQuery) > 0 {
+				queryPart = "?" + req.URL.RawQuery
+			}
+			w.Header().Set("Location", req.URL.Path+"/"+queryPart)
+			w.WriteHeader(http.StatusMovedPermanently)
+			return nil
+		}
+
+		// if transport == nil || wrapTransport {
+		// 	h.Transport = h.defaultProxyTransport(req.URL, h.Transport)
+		// }
+
+		transport, err := rest.TransportFor(config)
+		if err != nil {
+			log.Info("Could not get transport")
+			return err
+		}
+
+		log.Info(transport)
+
+		// WithContext creates a shallow clone of the request with the new context.
+		newReq := req.WithContext(req.Context())
+		//newReq := req.WithContext(context.Background())
+		newReq.Header = utilnet.CloneHeader(req.Header)
+		newReq.URL = loc
+
+		// Set auth header so we log in if needed
+		if len(tokenRec.AuthToken) > 0 {
+			newReq.Header.Add("Authorization", "Bearer "+tokenRec.AuthToken)
+			log.Info("Setting auth header")
+		}
+
+		proxy := httputil.NewSingleHostReverseProxy(&url.URL{Scheme: loc.Scheme, Host: loc.Host})
+		proxy.Transport = transport
+		proxy.FlushInterval = defaultFlushInterval
+		proxy.ModifyResponse = func(response *http.Response) error {
+			log.Debugf("GOT PROXY RESPONSE: %s", loc.String())
+			log.Debugf("%d", response.StatusCode)
+			log.Debug(response.Header.Get("Content-Type"))
+
+			log.Debugf("%v+", response.Header)
+			response.Header.Del("X-FRAME-OPTIONS")
+			response.Header.Set("X-FRAME-OPTIONS", "sameorigin")
+			log.Debug("%v+", response)
+			return nil
+		}
+
+		log.Errorf("Proxy: %s", target)
+
+		// Note that ServeHttp is non blocking and uses a go routine under the hood
+		proxy.ServeHTTP(w, newReq)
+
+		// We need this to be blocking
+
+		// select {
+		// case <-newReq.Context().Done():
+		// 	return newReq.Context().Err()
+		// }
+
+		log.Errorf("Finished proxying request: %s", target)
 	}
-
-	// if transport == nil || wrapTransport {
-	// 	h.Transport = h.defaultProxyTransport(req.URL, h.Transport)
-	// }
-
-	transport, err := rest.TransportFor(config)
-	if err != nil {
-		log.Info("Could not get transport")
-		return err
-	}
-
-	log.Info(transport)
-
-	// WithContext creates a shallow clone of the request with the new context.
-	newReq := req.WithContext(req.Context())
-	//newReq := req.WithContext(context.Background())
-	newReq.Header = utilnet.CloneHeader(req.Header)
-	newReq.URL = loc
-
-	// Set auth header so we log in if needed
-	if len(tokenRec.AuthToken) > 0 {
-		newReq.Header.Add("Authorization", "Bearer "+tokenRec.AuthToken)
-		log.Info("Setting auth header")
-	}
-
-	proxy := httputil.NewSingleHostReverseProxy(&url.URL{Scheme: loc.Scheme, Host: loc.Host})
-	proxy.Transport = transport
-	proxy.FlushInterval = defaultFlushInterval
-	proxy.ModifyResponse = func(response *http.Response) error {
-		log.Debugf("GOT PROXY RESPONSE: %s", loc.String())
-		log.Debugf("%d", response.StatusCode)
-		log.Debug(response.Header.Get("Content-Type"))
-
-		log.Debugf("%v+", response.Header)
-		response.Header.Del("X-FRAME-OPTIONS")
-		response.Header.Set("X-FRAME-OPTIONS", "sameorigin")
-		log.Debug("%v+", response)
-		return nil
-	}
-
-	log.Errorf("Proxy: %s", target)
-
-	// Note that ServeHttp is non blocking and uses a go routine under the hood
-	proxy.ServeHTTP(w, newReq)
-
-	// We need this to be blocking
-
-	// select {
-	// case <-newReq.Context().Done():
-	// 	return newReq.Context().Err()
-	// }
-
-	log.Errorf("Finished proxying request: %s", target)
-
 	return nil
 }
 
@@ -183,9 +216,9 @@ func (k *KubernetesSpecification) argoDashboardStatus(c echo.Context) error {
 		Installed: false,
 	}
 
-	pod, err := k.getKubeDashboardPod(c, "app%3Dkubernetes-dashboard")
+	pod, err := k.getKubeDashboardPod(c, "app%3Dargocd-server")
 	if err != nil {
-		pod, err = k.getKubeDashboardPod(c, "k8s-app%3Dkubernetes-dashboard")
+		pod, err = k.getKubeDashboardPod(c, "k8s-app%3Dargocd-server")
 	}
 
 	status.Pod = pod
